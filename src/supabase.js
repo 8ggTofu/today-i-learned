d65757eb3743bbbe724effca9446ffc0b07e0154
TQY <qingyuantay@gmail.com>
2023-07-03 11:29:25 +0800

    post .env file

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
+require('dotenv').config();
 
-const supabaseUrl = 'https://kxkddwiwxgjbluremxli.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4a2Rkd2l3eGdqYmx1cmVteGxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY1NTc2OTcsImV4cCI6MjAwMjEzMzY5N30.K-weCQCQkUSoisGjgX0V_fnPRqKAlXVGbNrbTBwvIk4"
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
